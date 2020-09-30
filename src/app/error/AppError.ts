619b11409c2e36c1ff8d362e0bcd39513b5982ab
WillCZhang <will.chi.zhang@gmail.com>
2020-09-30 13:42:33 -0700

    error handling refactoring

--- src/app/error/AppError.ts
+++ src/app/error/AppError.ts
@@ -1,12 +1,4 @@
 export default class AppError extends Error {
-    public message: any;
-    private language;
-
-    constructor(message: string) {
-        super();
-        this.message = message;
-    }
-
     public getMessage() {
         return this.message;
     }
--- src/app/error/RoomError.ts
+++ src/app/error/RoomError.ts
@@ -2,49 +2,25 @@ import {MAX_PLAYER, MIN_PLAYER} from "../Room";
 import AppError from "./AppError";
 
 export class RoomNotFoundError extends AppError {
-    static readonly msg = "房间已经被解散啦";
-
-    constructor() {
-        super(RoomNotFoundError.msg);
-    }
+    public readonly message = "房间已经被解散啦";
 }
 
 export class RoomExpireError extends AppError {
-    static readonly msg = "游戏已超时，一局游戏最长有效时间为一天 ：）";
-
-    constructor() {
-        super(RoomExpireError.msg);
-    }
+    public readonly message = "游戏已超时，一局游戏最长有效时间为一天 ：）";
 }
 
 export class RoomSizeError extends AppError {
-    static readonly msg = `一局游戏最少${MIN_PLAYER}名玩家参与，最多${MAX_PLAYER}名玩家参与`;
-
-    constructor() {
-        super(RoomSizeError.msg);
-    }
+    public readonly message = `一局游戏最少${MIN_PLAYER}名玩家参与，最多${MAX_PLAYER}名玩家参与`;
 }
 
 export class NonCreatorStartGameError extends AppError {
-    static readonly msg = "只有房主才能开始游戏哦";
-
-    constructor() {
-        super(NonCreatorStartGameError.msg);
-    }
+    public readonly message = "只有房主才能开始游戏哦";
 }
 
 export class PlayerCountError extends AppError {
-    static readonly msg = "还有玩家没有加入游戏，再等一等吧";
-
-    constructor() {
-        super(PlayerCountError.msg);
-    }
+    public readonly message = "还有玩家没有加入游戏，再等一等吧";
 }
 
 export class SeatTakenError extends AppError {
-    static readonly msg = "座位已经被抢啦，换一个试试吧";
-
-    constructor() {
-        super(SeatTakenError.msg);
-    }
+    public readonly message =  "座位已经被抢啦，换一个试试吧";
 }
